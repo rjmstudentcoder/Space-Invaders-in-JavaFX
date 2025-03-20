063444e1bc6d60f8ad48023f25fab67582945ccd
Roy M. <rmcneese@student.alamo.edu>
2025-03-20 15:43:37 -0500

    Removed ImageView.

--- src/com/roy/util/whisp/StencilImage.java
+++ src/com/roy/util/whisp/StencilImage.java
@@ -5,19 +5,15 @@ import javafx.scene.image.ImageView;
 
 public class StencilImage {
 	public Image image;
-	public ImageView imageView;
 	public Vector2 size;
 	public Vector2 spriteSize;
 	public Vector2 spriteOffset;
 
 	public void setOffset(Vector2 newOffset) {
 		spriteOffset = newOffset;
-		imageView.setViewport(
-				new javafx.geometry.Rectangle2D(spriteOffset.x, spriteOffset.y, spriteSize.x, spriteSize.y));
 	}
 
 	public StencilImage(Vector2 newSpriteSize, Image newImage) {
-		imageView = new ImageView(image);
 		spriteSize = newSpriteSize;
 		size = spriteSize;
 		setOffset(new Vector2(0, 0));
