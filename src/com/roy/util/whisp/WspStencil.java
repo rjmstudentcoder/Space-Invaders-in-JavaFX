134dc4d96a03f57073a1c85dc995e4aa5e653ea2
Roy M. <rmcneese@student.alamo.edu>
2025-03-20 14:35:22 -0500

    Update WspStencil.java
    Reformatted class.

--- src/com/roy/util/whisp/WspStencil.java
+++ src/com/roy/util/whisp/WspStencil.java
@@ -3,11 +3,11 @@ package com.roy.util.whisp;
 public interface WspStencil {
 
 	void drawImage(StencilImage image, Vector2 position, Vector2 size, int rotation);
-	
+
 	Vector2 getScreenResolution();
-	
+
 	Vector2 getScaleMultiplier();
-	
+
 	float getAspectRatio();
-	
+
 }
