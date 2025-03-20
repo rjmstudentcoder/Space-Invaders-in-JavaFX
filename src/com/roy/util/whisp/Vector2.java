5ed9754123ca38c4086202558232c709d8a2cfa9
Roy M. <rmcneese@student.alamo.edu>
2025-03-20 14:35:15 -0500

    Added copy constructor.

--- src/com/roy/util/whisp/Vector2.java
+++ src/com/roy/util/whisp/Vector2.java
@@ -3,12 +3,17 @@ package com.roy.util.whisp;
 public class Vector2 {
 	public float x;
 	public float y;
-	
+
 	public Vector2(float newX, float newY) {
 		this.x = newX;
 		this.y = newY;
 	}
 	
+	public Vector2(Vector2 b) {
+		this.x = b.x;
+		this.y = b.y;
+	}
+
 	public static Vector2 add(Vector2 a, Vector2 b) {
 		return new Vector2(a.x + b.x, a.y + b.y);
 	}
@@ -16,15 +21,15 @@ public class Vector2 {
 	public static Vector2 sub(Vector2 a, Vector2 b) {
 		return new Vector2(a.x - b.x, a.y - b.y);
 	}
-	
+
 	public static Vector2 mul(Vector2 a, Vector2 b) {
 		return new Vector2(a.x * b.x, a.y * b.y);
 	}
-	
+
 	public static Vector2 div(Vector2 a, Vector2 b) {
 		return new Vector2(a.x / b.x, a.y / b.y);
 	}
-	
+
 	public void tAdd(Vector2 b) {
 		this.x += b.x;
 		this.y += b.y;
