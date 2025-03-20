ad936d6541b50ca72ebdf93d0a802d2a910418d9
Roy M. <rmcneese@student.alamo.edu>
2025-03-20 15:43:25 -0500

    Added linear combat mechanics.

--- src/com/roy/games/invaders/Main.java
+++ src/com/roy/games/invaders/Main.java
@@ -25,9 +25,12 @@ public class Main extends Application implements WspStencil {
 	public Vector2 resolution;
 	public long clock;
 	public Canvas canvas;
+	int selectedAlien;
 	GraphicsContext graphics_context;
+	Image laserImage;
 	Player player;
 	ArrayList<Alien> aliens;
+	ArrayList<Laser> lasers;
 
 	public void drawImage(StencilImage image, Vector2 position, Vector2 size, int rotation) {
 		graphics_context.drawImage(image.image, image.spriteOffset.x, image.spriteOffset.y, image.spriteSize.x,
@@ -51,7 +54,52 @@ public class Main extends Application implements WspStencil {
 		for (int i=0; i < aliens.size(); i++) {
 			Alien alien = aliens.get(i);
 			alien.update(dt);
+			if (alien.deathTick > 0.3) {
+				aliens.remove(alien);
+				break;
+			}
+			if (!alien.dead && alien.laserTick > 1) {
+				alien.laserTick = 0;
+				lasers.add(new Laser(new Vector2(Vector2.add(alien.position, new Vector2(0, 5))), new Vector2(0, 20), laserImage, new Vector2(3, 8), new Vector2(0, 0), true));
+			}
 		}
+		boolean shouldExitLoop = false;
+		for (int i=0; i < lasers.size(); i++) {
+			if (shouldExitLoop) {
+				break;
+			}
+			Laser laser = lasers.get(i);
+			laser.update(dt);
+			if (laser.deathTick > 0.25) {
+				lasers.remove(laser);
+				break;
+			}
+			if (!laser.dead && laser.evil == false) {
+				for (int i2=0; i2 < aliens.size(); i2++) {
+					Alien alien = aliens.get(i2);
+					Vector2 distance = Vector2.sub(alien.position, laser.position);
+					if (Math.abs(distance.x) < alien.image.spriteSize.x/2 && Math.abs(distance.y) < alien.image.spriteSize.y/2) {
+						//aliens.remove(alien);
+						//lasers.remove(laser);
+						alien.dead = true;
+						laser.dead = true;
+						shouldExitLoop = true;
+						break;
+					}
+				}
+			}
+		}
+
+		if (selectedAlien < aliens.size()) {
+			if (selectedAlien == -1 || aliens.get(selectedAlien).laserTick == 0) {
+				selectedAlien = (int) (Math.random() * aliens.size());
+				aliens.get(selectedAlien).laserTick = (float)0.1;
+			}
+			aliens.get(selectedAlien).laserTick += dt;
+		} else {
+			selectedAlien = (int) (Math.random() * aliens.size());
+		}
+	
 	}
 
 	public void draw(double dt) {
@@ -60,6 +108,10 @@ public class Main extends Application implements WspStencil {
 			Alien alien = aliens.get(i);
 			drawImage(alien.image, alien.position, alien.image.size, 0);
 		}
+		for (int i=0; i < lasers.size(); i++) {
+			Laser laser = lasers.get(i);
+			drawImage(laser.image, laser.position, laser.image.size, 0);
+		}
 		graphics_context.setFill(Color.WHITE);
 		graphics_context.fillRect(0, 0, resolution.x * scaleMultiplier.x, 1 * scaleMultiplier.y);
 	}
@@ -67,7 +119,7 @@ public class Main extends Application implements WspStencil {
 	public void start(Stage stage) {
 		stage.setTitle("Space Invaders");
 		
-		scaleMultiplier = new Vector2(5, 5);
+		scaleMultiplier = new Vector2(3, 3);
 		
 		resolution = new Vector2(192, 160);
 		aspectRatio = resolution.y / resolution.x;
@@ -86,13 +138,16 @@ public class Main extends Application implements WspStencil {
 		stage.setResizable(false);
 		stage.setScene(scene);
 		stage.show();
-
+		
 		player = new Player(new Vector2(resolution.x / 2, resolution.y - 20), 0, resolution.x);
+		lasers = new ArrayList<Laser>();
+		laserImage = new Image(getClass().getResource("/assets/images/sprites/lasers.png").toExternalForm());
+		selectedAlien = -1;
 		aliens = new ArrayList<Alien>();
 		Image alienImage = new Image(getClass().getResource("/assets/images/sprites/aliens.png").toExternalForm());
 		for (int y=1; y < 4; y++) {
 			for (int x=1; x < 10; x++) {
-				Alien newAlien = new Alien(new Vector2(x * 20 - 5, y * 30), alienImage, new Vector2(16, 8));
+				Alien newAlien = new Alien(new Vector2(x * 20 - 5, y * 20), alienImage, new Vector2(16, 8));
 				if (y > 1) {
 					newAlien.motionAngle += (Math.PI  / 2) * y +( x )  / 2;
 				}
@@ -110,6 +165,8 @@ public class Main extends Application implements WspStencil {
 					player.controlDeltaX = -1;
 				} else if (code == KeyCode.RIGHT && player.controlDeltaX != -1) {
 					player.controlDeltaX = 1;
+				} else if (code == KeyCode.Z || code == KeyCode.SPACE) {
+					lasers.add(new Laser(new Vector2(Vector2.add(player.position, new Vector2(0, -5))), new Vector2(0, -20), laserImage, new Vector2(3, 8), new Vector2(24, 0), false));
 				}
 			}
 		});
