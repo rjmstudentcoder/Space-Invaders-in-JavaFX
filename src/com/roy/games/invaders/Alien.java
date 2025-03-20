5ce05ec088795dbecec2430c7fd643284a198bf2
Roy M. <rmcneese@student.alamo.edu>
2025-03-20 15:43:05 -0500

    Added event animations.

--- src/com/roy/games/invaders/Alien.java
+++ src/com/roy/games/invaders/Alien.java
@@ -10,26 +10,38 @@ public class Alien {
 	Vector2 position;
 	float motionAngle;
 	StencilImage image;
-	float spriteSwapTick = 0;
+	float spriteSwapTick;
+	float laserTick;
+	boolean dead;
+	double deathTick;
 	
 	Alien(Vector2 newOrigin, Image newImage, Vector2 imageSize) {
 		position = newOrigin;
 		origin = new Vector2(newOrigin);
 		image = new StencilImage(imageSize, newImage);
+		spriteSwapTick = 0;
+		laserTick = 0;
+		dead = false;
+		deathTick = 0;
 	}
 
 	void update(double dt) {
-		spriteSwapTick += dt;
-		if (spriteSwapTick > 1) {
-			spriteSwapTick = 0;
-			if (image.spriteOffset.y < 1) {
-				image.setOffset(new Vector2(image.spriteOffset.x, image.spriteSize.y));
-			} else {
-				image.setOffset(new Vector2(image.spriteOffset.x, 0));
+		if (dead) {
+			deathTick += dt;
+			image.setOffset(new Vector2(48, 0));
+			image.spriteSize = new Vector2(16, 8);
+		} else {
+			spriteSwapTick += dt;
+			if (spriteSwapTick > 1) {
+				spriteSwapTick = 0;
+				if (image.spriteOffset.y < 1) {
+					image.setOffset(new Vector2(image.spriteOffset.x, image.spriteSize.y));
+				} else {
+					image.setOffset(new Vector2(image.spriteOffset.x, 0));
+				}
 			}
+			motionAngle +=dt * 2;
+			position.x = (float) (origin.x + (1 - Math.abs((motionAngle / Math.PI) % 2 - 1)) * 10);
 		}
-		motionAngle +=dt * 2;
-		
-		position.x = (float) (origin.x + (1 - Math.abs((motionAngle / Math.PI) % 2 - 1)) * 10);
 	}
 }
