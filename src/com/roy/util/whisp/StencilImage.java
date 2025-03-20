c667b14cae065a4c88dec1ba7ee3611b52c56b11
Roy M. <rmcneese@student.alamo.edu>
2025-03-20 14:35:05 -0500

    Reformatted class.

--- src/com/roy/util/whisp/StencilImage.java
+++ src/com/roy/util/whisp/StencilImage.java
@@ -9,10 +9,13 @@ public class StencilImage {
 	public Vector2 size;
 	public Vector2 spriteSize;
 	public Vector2 spriteOffset;
+
 	public void setOffset(Vector2 newOffset) {
 		spriteOffset = newOffset;
-		imageView.setViewport(new javafx.geometry.Rectangle2D(spriteOffset.x, spriteOffset.y, spriteSize.x, spriteSize.y));
+		imageView.setViewport(
+				new javafx.geometry.Rectangle2D(spriteOffset.x, spriteOffset.y, spriteSize.x, spriteSize.y));
 	}
+
 	public StencilImage(Vector2 newSpriteSize, Image newImage) {
 		imageView = new ImageView(image);
 		spriteSize = newSpriteSize;
