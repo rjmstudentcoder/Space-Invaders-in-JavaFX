1255bd9fafa3818346689febddefc3a5ed141098
Roy M. <rmcneese@student.alamo.edu>
2025-03-20 17:17:25 -0500

    Added player death animations.

--- src/com/roy/games/invaders/Player.java
+++ src/com/roy/games/invaders/Player.java
@@ -1,27 +1,37 @@
 package com.roy.games.invaders;
 
-import com.roy.util.whisp.StencilImage;
 import com.roy.util.whisp.Vector2;
 
 import javafx.scene.image.Image;
 
-public class Player {
-	public StencilImage image;
-	public Vector2 position;
+public class Player extends Entity {
 	public int controlDeltaX = 0;
 	public float minX;
 	public float maxX;
 
-	public Player(Vector2 newPos, float newMinX, float newMaxX) {
-		image = new StencilImage(new Vector2(16, 8),
-				new Image(getClass().getResource("/assets/images/sprites/player.png").toExternalForm()));
-		position = newPos;
+	public Player(Image newImage, Vector2 newPos, float newMinX, float newMaxX) {
+		super(newPos, Vector2.zero(), newImage,  new Vector2(16, 8), Vector2.zero());
 		minX = newMinX;
 		maxX = newMaxX;
+		setAnimationInfo(0.5, 2, 0, false);
+	}
+	
+	public void kill() {
+		super.kill();
+		image.setOffset(new Vector2(16, 0));
 	}
 
 	public void update(double dt) {
-		float difference = (float) (position.x + (controlDeltaX * dt * 100));
-		position.x = Math.min(Math.max(difference, minX), maxX);
+		super.update(dt);
+		if (!dying) {
+			float difference = (float) (position.x + (controlDeltaX * dt * 100));
+			position.x = Math.min(Math.max(difference, minX), maxX);
+		} else {
+			if (deathTick > afterlife / 3 && deathTick < (afterlife / 3) * 2 ) {
+				image.setOffset(new Vector2(32, 0));
+			} else if (deathTick > (afterlife / 3) * 2) {
+				image.setOffset(new Vector2(48, 0));
+			}
+		}
 	}
 }
