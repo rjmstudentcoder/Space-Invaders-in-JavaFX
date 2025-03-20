db7c3ab859e445817dab1532d8fb61998e412fd8
Roy M. <rmcneese@student.alamo.edu>
2025-03-20 00:24:41 -0500

    Update module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module com.roy.games.invaders {
 	requires transitive javafx.base;
 	requires transitive javafx.graphics;
+	requires transitive javafx.controls;
 	exports com.roy.games.invaders;
 	exports com.roy.util.whisp;
 }
