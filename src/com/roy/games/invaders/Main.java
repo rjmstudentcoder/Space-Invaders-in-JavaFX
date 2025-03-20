ce19cf56177efc3711358adf03839171f74a6dc6
Roy M. <rmcneese@student.alamo.edu>
2025-03-19 22:52:37 -0500

    Added AnimationTimer.

--- src/com/roy/games/invaders/Main.java
+++ src/com/roy/games/invaders/Main.java
@@ -3,6 +3,7 @@ package com.roy.games.invaders;
 import com.roy.util.whisp.WspStencil;
 import com.roy.util.whisp.Vector2;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application; 
 import javafx.scene.Scene; 
 //import javafx.scene.control.*; 
@@ -18,6 +19,7 @@ public class Main extends Application implements WspStencil {
 	public Vector2 scaleMultiplier;
 	public float aspectRatio;
 	public Vector2 resolution;
+	public long clock;
 
 	public void drawImage(String image, Vector2 position, Vector2 size, int rotation) {
 		
@@ -34,45 +36,45 @@ public class Main extends Application implements WspStencil {
 	public float getAspectRatio() {
 		return this.aspectRatio;
 	};
-	
+
 	 public void start(Stage stage) 
 	    { 
-	        // set title for the stage 
 	        stage.setTitle("creating canvas"); 
-	  
-	        // create a canvas 
+
 	        Canvas canvas = new Canvas(); 
-	  
-	        // set height and width 
-	        canvas.setHeight(400); 
-	        canvas.setWidth(400); 
-	  
-	        // graphics context 
-	        GraphicsContext graphics_context =  
-	            canvas.getGraphicsContext2D(); 
-	  
-	        // set fill for rectangle 
-	        graphics_context.setFill(Color.PINK); 
-	        graphics_context.fillRect(40, 40, 100, 100); 
-	  
-	        // set fill for rectangle 
-	        graphics_context.setFill(Color.RED); 
-	        graphics_context.fillRect(20, 20, 70, 70); 
-	  
-	        // set fill for oval 
-	        graphics_context.setFill(Color.BLUE); 
-	        graphics_context.fillOval(30, 30, 70, 70); 
-	  
-	        // create a Group 
+	        canvas.setHeight(160); 
+	        canvas.setWidth(192); 
+
+	        GraphicsContext graphics_context =  canvas.getGraphicsContext2D(); 
+	 
+	        graphics_context.setFill(Color.BLACK); 
+	        graphics_context.fillRect(0, 0, 192, 160); 
+
 	        Group group = new Group(canvas); 
-	  
-	        // create a scene 
-	        Scene scene = new Scene(group, 400, 400); 
-	  
-	        // set the scene 
+	        Scene scene = new Scene(group, 192, 160); 
 	        stage.setScene(scene); 
-	  
 	        stage.show(); 
+	        
+	       
+	        clock = System.nanoTime();
+	        AnimationTimer animator = new AnimationTimer() {
+	        	 float i = 0;
+	            @Override
+	            public void handle(long arg0) {
+	                long current_time = System.nanoTime();
+	                double dt = (double) ((current_time - clock) / 1000000000.0);
+	                if (0.03 <= dt) {
+	                	i += dt * 100;
+	                	System.out.println(dt);
+		   	        	graphics_context.setFill(Color.WHITE); 
+		   	 	       graphics_context.fillRect(0, 0, 192, i); 
+	                	clock = current_time;
+	                }
+	                
+	            }
+	        };
+	        animator.start();
+	  
 	    } 
 }
 
