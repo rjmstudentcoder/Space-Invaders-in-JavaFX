b0aefda6d9dab7de71d0891f852722a754d0c98d
Roy M. <rmcneese@student.alamo.edu>
2025-03-20 14:34:55 -0500

    Reformatted class.

--- src/com/roy/games/invaders/Player.java
+++ src/com/roy/games/invaders/Player.java
@@ -6,21 +6,22 @@ import com.roy.util.whisp.Vector2;
 import javafx.scene.image.Image;
 
 public class Player {
-	StencilImage image;
-	Vector2 position;
+	public StencilImage image;
+	public Vector2 position;
 	public int controlDeltaX = 0;
 	public float minX;
 	public float maxX;
-	
+
 	public Player(Vector2 newPos, float newMinX, float newMaxX) {
-		image = new StencilImage(new Vector2(16, 8), new Image(getClass().getResource("/assets/images/sprites/player.png").toExternalForm()));
+		image = new StencilImage(new Vector2(16, 8),
+				new Image(getClass().getResource("/assets/images/sprites/player.png").toExternalForm()));
 		position = newPos;
 		minX = newMinX;
 		maxX = newMaxX;
 	}
-	
+
 	public void update(double dt) {
-		float difference = (float)(position.x + (controlDeltaX *  dt * 100));
+		float difference = (float) (position.x + (controlDeltaX * dt * 100));
 		position.x = Math.min(Math.max(difference, minX), maxX);
 	}
 }
