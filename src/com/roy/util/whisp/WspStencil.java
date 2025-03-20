93644e9ef4514475a35c9d24c324893659709061
Roy M. <rmcneese@student.alamo.edu>
2025-03-20 17:17:35 -0500

    Removed unused methods.

--- src/com/roy/util/whisp/WspStencil.java
+++ src/com/roy/util/whisp/WspStencil.java
@@ -4,10 +4,4 @@ public interface WspStencil {
 
 	void drawImage(StencilImage image, Vector2 position, Vector2 size, int rotation);
 
-	Vector2 getScreenResolution();
-
-	Vector2 getScaleMultiplier();
-
-	float getAspectRatio();
-
 }
